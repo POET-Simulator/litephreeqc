ecba265f2eb183ca53cbe1fb6510c3f8ef1057f1
Max Luebke <mluebke@gfz-potsdam.de>
2024-09-18 20:25:17 +0200

    Improve usability outside POET

--- poet/include/PhreeqcEngine.hpp
+++ poet/include/PhreeqcEngine.hpp
@@ -1,16 +1,15 @@
 #pragma once
 
-#include "POETInit.hpp"
-
+#include "PhreeqcMatrix.hpp"
 #include <memory>
 #include <vector>
 
 /**
  * @brief Class for running Phreeqc wrappped in POET
  *
- * Direct interface to Phreeqc, without utilizing *_MODIFY keywords/scripts
- to
- * set new values. Use with already initialized Phreeqc config.
+ * Direct interface to Phreeqc, without utilizing Phreeqc's internal parser
+ * to set/get values. To start a simulation, the interpreter is used internally.
+ * No need for the user to directly interact with the interpreter.
  *
  */
 class PhreeqcEngine {
@@ -19,11 +18,13 @@ public:
    * @brief Construct a new Phreeqc Engine object
    *
    * Construct a new Phreeqc Engine object by previously initialized
-   POETConfig.
+   * PhreeqcMatrix.
    *
-   * @param config Holds the configuration for the Phreeqc engine.
+   * @param pqc_mat PhreeqcMatrix initialized with the *full* Phreeqc script and
+   * database.
+   * @param cell_id ID of the cell (user id from Phreeqc script) to simulate
    */
-  PhreeqcEngine(const POETConfig &config);
+  PhreeqcEngine(const PhreeqcMatrix &pqc_mat, const int cell_id);
 
   /**
    * @brief Destroy the Phreeqc Engine object
@@ -35,8 +36,8 @@ public:
    * @brief Siimulate a cell for a given time step
    *
    * @param cell_values Vector containing the input values for the cell
-   * (**including the ID**). Output values are written back in place to this
-   * vector!
+   * (*including the ID*). Output values are written back in place to this
+   * vector.
    * @param time_step Time step to simulate in seconds
    */
   void runCell(std::vector<double> &cell_values, double time_step);
