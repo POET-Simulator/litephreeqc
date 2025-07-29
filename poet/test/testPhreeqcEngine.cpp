b7c2574a0decaac190a1efe254700b9bd9c39853
Max Luebke <mluebke@gfz-potsdam.de>
2025-07-29 14:53:15 +0200

    test: adapt tests to new API

--- poet/test/testPhreeqcEngine.cpp
+++ poet/test/testPhreeqcEngine.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 
+#include "IPhreeqcReader.hpp"
 #include "PhreeqcEngine.hpp"
 #include "PhreeqcMatrix.hpp"
 #include "utils.hpp"
@@ -23,19 +24,28 @@ POET_TEST(PhreeqcEngineStep) {
 
   PhreeqcEngine engine(pqc_mat, 1);
 
+  IPhreeqcReader pqc_compare(test_database, base_test::script);
+
   std::vector<double> cell_values = pqc_mat.get().values;
+  std::vector<std::string> cell_names = pqc_mat.get().names;
   cell_values.erase(cell_values.begin(), cell_values.begin() + 1);
+  cell_names.erase(cell_names.begin(), cell_names.begin() + 1);
 
   EXPECT_NO_THROW(engine.runCell(cell_values, 0));
   EXPECT_NO_THROW(engine.runCell(cell_values, 100));
 
-  for (std::size_t i = 0; i < cell_values.size(); ++i) {
-    // skip Charge, H(0) and O(0)
-    if (i >= 2 && i <= 4) {
+  pqc_compare.run(0, {1});
+  pqc_compare.run(100, {1});
+
+  pqc_compare.setOutputID(1);
+
+  for (std::size_t i = 0; i < cell_names.size(); ++i) {
+    // Somehow 'pe' will not result in a expected near value, therefore we skip
+    // it
+    if (cell_names[i] == "pe") {
       continue;
     }
-    EXPECT_NEAR(cell_values[i], base_test::expected_values[i + 1],
-                base_test::expected_errors[i + 1]);
+    EXPECT_NEAR(cell_values[i], pqc_compare[cell_names[i]], 1e-6);
   }
 
   EXPECT_THROW(engine.runCell(cell_values, -1), std::invalid_argument);
